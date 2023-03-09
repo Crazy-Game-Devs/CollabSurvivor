a75fc66c0d815f363bf1a011fc21152d7dfc3e98
Marlon Chow <marlon-kenneth.chow@dlr.de>
2023-03-09 20:09:00 +0100

    Limit walkable area implemented + "camera" renamed

--- Assets/Code/Player/PlayerMovement.cs
+++ Assets/Code/Player/PlayerMovement.cs
@@ -5,26 +5,29 @@ using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-	[SerializeField]
-	private float playerMovement = 1.0f;
-	[SerializeField]
-	private GameObject camera;
+    [SerializeField]
+    private float playerMovement = 1.0f;
+    [SerializeField]
+    private GameObject userView;
+    [SerializeField]
+    private Vector2 mapSize = new(30f, 30f);
 
-	// Start is called before the first frame update
-	void Start()
-	{
+    // Start is called before the first frame update
+    void Start()
+    {
 
-	}
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-		Vector2 input = Vector2.zero;
-		input.y = Input.GetAxisRaw("Vertical");
-		input.x = Input.GetAxisRaw("Horizontal");
-		input = input.normalized * playerMovement * Time.deltaTime;
-		transform.position = new Vector3(transform.position.x + input.x, transform.position.y + input.y, transform.position.z);
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = Vector2.zero;
+        input.y = Input.GetAxisRaw("Vertical");
+        input.x = Input.GetAxisRaw("Horizontal");
+        input = playerMovement * Time.deltaTime * input.normalized;
 
-		camera.transform.position = new Vector3(transform.position.x, transform.position.y, camera.transform.position.z);
-	}
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x + input.x, -mapSize.x, mapSize.x), Mathf.Clamp(transform.position.y + input.y, -mapSize.y, mapSize.y), transform.position.z);
+
+        userView.transform.position = new Vector3(transform.position.x, transform.position.y, userView.transform.position.z);
+    }
 }
