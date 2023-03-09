9df77a18e0b588cfc63c5f5bde51aa3e58cdbb99
Marlon Chow <marlon-kenneth.chow@dlr.de>
2023-03-09 19:40:14 +0100

    Basic interfaces for different itemtypes implemented

---
+++ Assets/Code/Items/WeaponInterface.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface WeaponInterface : BaseItemInterface
+{
+    public bool OnAttackmove();
+}
--- Assets/Code/Player/BasePlayerStats.cs
+++ Assets/Code/Player/BasePlayerStats.cs
@@ -7,6 +7,9 @@ public class BasePlayerStats : BaseCharakterStats
 	[SerializeField]
 	private int maxWeaponAmount;
 
-	// getter setter
-	public int MaxWeaponAmount { get => maxWeaponAmount; protected set => maxWeaponAmount = value; }
+    [SerializeField]
+    private float attack;
+
+    // getter setter
+    public int MaxWeaponAmount { get => maxWeaponAmount; protected set => maxWeaponAmount = value; }
 }
